ad5b60b24c69396fc7d2c3c06fb1aad6b0f4ade0
YuYangHP <42936279+YuYangHP@users.noreply.github.com>
2021-10-19 15:35:03 +0800

    Add files via upload

---
+++ httputil.js
@@ -0,0 +1,22 @@
+let BASE_Server = "http://121.41.107.125:8080/BookMybatis_war/"
+//let BASE_Server = "http://192.168.110.114:8000/BookMybatis_war_exploded/"
+// let BASE_Server = "http://192.168.2.127:80/BookMybatis_war_exploded/"
+// let BASE_Server = "http://10.63.1.57:8080/BookMybatis/"
+function send_post(url, data, successfun) {
+  wx.request({
+    url: BASE_Server + url, //仅为示例，并非真实的接口地址
+    method: "POST",
+    header: {
+      'content-type': 'application/x-www-form-urlencoded'
+    },
+    data: data,
+    success(res) {
+      // console.log(res)
+      successfun(res)
+    }
+  })
+}
+module.exports = {
+  BASE_Server: BASE_Server,
+  send_post: send_post
+}
